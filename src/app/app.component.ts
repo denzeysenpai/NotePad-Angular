fecefd509336828c1b61612f26cee187d7d49c81
DenzeySenpai <marksolante221@gmail.com>
2024-06-04 17:21:06 +0800

    Started project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskInputComponent } from './task-input/task-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TaskInputComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
